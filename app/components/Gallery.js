a0b26632ffea867c646cfe570b22a79651242a1a
Kenny Nagano <nagano564@gmail.com>
2018-06-18 23:05:56 -0400

    working with google api

---
+++ app/components/Gallery.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Gallery extends Component {
+  render() {
+    return (
+      <div>
+        {
+          this.props.items.map(item => {
+            let { title } = item.volumeInfo
+            return (
+              <div>{title}</div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default Gallery;
--- app/components/Global.js
+++ app/components/Global.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import Gallery from './Gallery';
 
 class Global extends Component {
-  search(){
-    console.log('search');
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      items: []
+    }
+  }
+
+  search() {
+    const BASE_URL = "https://www.googleapis.com/books/v1/volumes?q="
+    fetch(`${BASE_URL}${this.state.query}`, { method: 'GET'} )
+      .then(response => response.json())
+      .then(json => {
+        let { items } = json;
+        this.setState({items})
+      });
   }
 
   render(){
     return (
-      <div>
+      <div className="Global">
         <h2>Music Search!</h2>
         <FormGroup>
           <InputGroup>
             <FormControl
               type="text"
               placeholder="Search for Music"
+              onChange={event => this.setState({query: event.target.value})}
+              onKeyPress={event => {
+                if (event.key === "Enter") {
+                  this.search();
+                }
+              }}
             />
             <InputGroup.Addon onClick={() => this.search()}>
               <Glyphicon glyph="search"></Glyphicon>
             </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
+        <Gallery items={this.state.items}/>
       </div>
     )
   }
